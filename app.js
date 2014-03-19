3348986593f4aeb9ecda2f2f521d637e79b6ceac
zhewuzhou <zhewuzhou@gmail.com>
2014-03-19 21:38:34 +0800

    add index.html page

--- app.js
+++ app.js
@@ -8,10 +8,9 @@ var http = require('http');
 
 var app = express();
 
-app.get('/hello', function(req, res){
-  res.send('Hello World!');
-});
+app.use('/', express.static(__dirname));
 
 http.createServer(app).listen(8080, function(){
   console.log('Express server listening on port 8080');
 });
+
